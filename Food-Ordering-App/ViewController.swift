e7978e4bae5dcdaebfd1b8e86557840483ec7254
rustemmanafov <rustemmanafov1@gmail.com>
2022-07-24 15:30:27 +0400

    create new app

---
+++ Food-Ordering-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Ordering-App
+//
+//  Created by Rustem Manafov on 24.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
