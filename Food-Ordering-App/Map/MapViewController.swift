d848930cc35fa9458b8375307fe5c4cad3dbfa22
rustemmanafov <rustemmanafov1@gmail.com>
2022-07-26 02:42:32 +0400

    updated

--- Food-Ordering-App/Map/MapViewController.swift
+++ Food-Ordering-App/Map/MapViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var latitude = ""
+    var longitude = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        manager.stopUpdatingLocation()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: Double(latitude) ?? 0,
+                                                longitude: Double(longitude) ?? 0)
+        let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pin.title = "I am here"
+        mapView.addAnnotation(pin)
+        
+        
     }
     
 
