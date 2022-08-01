811ed194b06c036dbb056976c8684152a09cc45c
rustemmanafov <rustemmanafov1@gmail.com>
2022-08-01 17:56:55 +0400

    l

--- Food-Ordering-App/Search/SearchViewController.swift
+++ Food-Ordering-App/Search/SearchViewController.swift
@@ -14,77 +14,67 @@ class SearchViewController: UIViewController {
     
     var searchRestaurant = [RestaurantModel]()
     var searchDishes = [DishesModel]()
-    
-    var data = ["alma", "heyva", "qarpiz"]
-    
-    var filteredData: [String]!
+        
+    var filteredData = [String]()
     var searching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      //  jsonSetup()
-        
-    filteredData = data
-        
+        jsonSetup()
     }
     
-//    func jsonSetup() {
-//        if let jsonFile = Bundle.main.url(forResource: "Restaurant", withExtension: "json"), let data = try? Data(contentsOf: jsonFile){
-//            do {
-//                searchRestaurant = try JSONDecoder().decode([RestaurantModel].self, from: data)
-//                tableView.reloadData()
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-//    }
-    
+    func jsonSetup() {
+        if let jsonFile = Bundle.main.url(forResource: "Restaurant", withExtension: "json"), let data = try? Data(contentsOf: jsonFile){
+            do {
+                searchRestaurant = try JSONDecoder().decode([RestaurantModel].self, from: data)
+                tableView.reloadData()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
-  
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-   
-        data.count
-//        if searching {
-//           return filteredData.count
-//        } else {
-//         return searchRestaurant.count
-//        }
+        
+        if searching {
+            return filteredData.count
+        } else {
+            return searchRestaurant.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
-
-                  cell.searchLabel.text = data[indexPath.row]
-     
-
-        //        if searching {
-//            cell.searchLabel.text = filteredData[indexPath.row]
-//        } else {
-//            cell.searchLabel.text = searchRestaurant[indexPath.row].name
-//        }
+        
+        if searching {
+            cell.searchLabel.text = filteredData[indexPath.row]
+        } else {
+            cell.searchLabel.text = searchRestaurant[indexPath.row].name
+        }
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let controller = storyboard?.instantiateViewController(withIdentifier: "RestoranDetailsViewController") as! RestoranDetailsViewController
-//        controller.restaurantDetail = searchRestaurant[indexPath.row]
-//        show(controller, sender: nil)
-//    }
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        let controller = storyboard?.instantiateViewController(withIdentifier: "RestoranDetailsViewController") as! RestoranDetailsViewController
+    //        controller.restaurantDetail = searchRestaurant[indexPath.row]
+    //        show(controller, sender: nil)
+    //    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-
-        filteredData = data.filter({ $0.contains(searchText)})
-        searching = true
-        self.tableView.reloadData()
-
+        
+//        filteredData = searchRestaurant.filter({$0.contains(searchText)})
+//        searching = true
+//        self.tableView.reloadData()
+//
     }
-
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searching = false
         searchBar.text = ""
